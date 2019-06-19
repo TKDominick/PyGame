8c0ee1d5161b992eb7102298de0b579223ee2812
Dominick Long <domaidlong@gmail.com>
2019-06-19 07:57:26 -0500

    More PyGame testing

--- scratch.py
+++ scratch.py
@@ -5,11 +5,13 @@ win = pygame.display.set_mode((1000,1000))
 
 pygame.display.set_caption("Test Game")
 
-x=50
-y=50
+x=0
+y=0
 width = 40
 height = 60
 vel = 15
+isJump = False
+jumpCount = 10
 
 run = True
 while run:
@@ -21,16 +23,31 @@ while run:
 		
 	keys = pygame.key.get_pressed()
 	
-	if keys[pygame.K_LEFT]:
+	if keys[pygame.K_LEFT] and x > vel - 1:
 		x -= vel
-	if keys[pygame.K_RIGHT]:
+	if keys[pygame.K_RIGHT] and x < 1000 - width - vel + 10:
 		x += vel
-	if keys[pygame.K_UP]:
-		y -= vel
-	if keys[pygame.K_DOWN]:
-		y+= vel
+	if not(isJump):
+		if keys[pygame.K_UP] and y > vel - 1:
+			y -= vel
+		if keys[pygame.K_DOWN] and y < 1000 - height - vel + 10:
+			y+= vel
+		if keys[pygame.K_SPACE]:
+			isJump = True
+	else:
+		if jumpCount >= -10:
+			neg = 1
+			if jumpCount < 0:
+				neg = -1
+			y -= (jumpCount ** 2) * 0.75 * neg
+			jumpCount -= 1
+		else:
+			isJump = False
+			jumpCount = 10
 		
-	win.fill  ((0,0,0))
+		
+		
+	win.fill  ((0,0,255))
 	pygame.draw.rect(win, (255,0,0), (x,y, width, height))
 	pygame.display.update()
 
