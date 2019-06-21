a4f26f11a81274306382a76436906fa80ea48e87
Dominick Long <domaidlong@gmail.com>
2019-06-21 08:33:02 -0500

    Added red baron to game, started with character drawing/animation groundwork

--- scratch.py
+++ scratch.py
@@ -1,21 +1,59 @@
 import pygame
+import time
 pygame.init()
 
 win = pygame.display.set_mode((1000,1000))
 
 pygame.display.set_caption("Test Game")
 
+Left = False
+Right = False
+Up = False
+Down = False
+
+flyleft = []
+flyright = []
+#bg = pygame.image.load('background.png') #Not done yet
+char = pygame.image.load('RedBaron.png')
+framecount = 0
+
 x=0
 y=0
-width = 40
-height = 60
+width = 64
+height = 64
 vel = 15
 isJump = False
 jumpCount = 10
 
+def redrawGameWindow():
+	global framecount
+	
+	#win.blit(bg, (0,0))
+	win.fill((135, 206, 250))
+	
+	if framecount + 1 >= 12:
+		framecount = 0
+		
+	#Character animations
+	'''
+	if Left:
+		win.blit(flyleft[framecount])
+	elif Right:
+		win.Blit(flyright[framecount])
+	else:
+	'''
+	win.blit(char, (x,y))
+	
+	
+	pygame.display.update()
+	
+
+
+
 run = True
 while run:
-	pygame.time.delay(20)
+	#clock.tick(27)
+	pygame.time.delay(16)
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -25,8 +63,24 @@ while run:
 	
 	if keys[pygame.K_LEFT] and x > vel - 1:
 		x -= vel
-	if keys[pygame.K_RIGHT] and x < 1000 - width - vel + 10:
+		Left = True
+		Right = False
+		Up = False
+		Down = False
+	elif keys[pygame.K_RIGHT] and x < 1000 - width - vel + 10:
 		x += vel
+		Left = False
+		Right = True
+		Up = False
+		Down = False
+	else:
+		Left = False
+		Right = False
+		Up = False
+		Down = False
+		framecount = 0
+		
+	# Remove Jump Function
 	if not(isJump):
 		if keys[pygame.K_UP] and y > vel - 1:
 			y -= vel
@@ -45,10 +99,7 @@ while run:
 			isJump = False
 			jumpCount = 10
 		
-		
-		
-	win.fill  ((0,0,255))
-	pygame.draw.rect(win, (255,0,0), (x,y, width, height))
-	pygame.display.update()
+	redrawGameWindow()
+	
 
 pygame.quit()
