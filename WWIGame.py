ee2685eb2015f43f5fe78d54a4eae1df89e531d4
Dominick Long <domaidlong@gmail.com>
2019-06-24 12:00:43 -0500

    added temp explosion sprite and grenade sprite, added collision detection, added enemy attack, added explosion effect

--- WWIGame.py
+++ WWIGame.py
@@ -16,8 +16,10 @@ bg = pygame.image.load('Background.png') # Not done yet
 
 framecount = 0
 
-
+ExplosionFramecount = 0
+explosions = []
 enemies = []
+enemyattacks = []
 bullets = []
 bombs = []
 BulletOnCD = False
@@ -57,54 +59,88 @@ class Projectile:
 		
 		if projtype == 1:
 			self.damage = 3
-			self.xvel = 20
+			self.xvel = 30
 			self.yvel = 00
 			self.move = 1		# straight
 			self.sprite = pygame.image.load(sprite)
 			
 		if projtype == 2:
 			self.damage = 15
-			self.xvel = 15
+			self.xvel = 20
 			self.yvel = 0
+			self.yaccel = 5
 			self.move = 2
 			self.sprite = pygame.image.load(sprite)
-			
+		
+		if projtype == 3:
+			self.damage = 2
+			self.xvel = -12
+			self.yvel = -25
+			self.yaccel = 5
+			self.move = 3
+			self.sprite = pygame.image.load(sprite)
+		
+		
 	def Move(self):
 		if self.move == 1:
 			self.x += self.xvel
 			self.y += self.yvel
+		
 		elif self.move == 2:
 			self.x += self.xvel
 			self.y += self.yvel
-			self.yvel += 5
+			self.yvel += self.yaccel
+		
+		elif self.move == 3:
+			self.x += self.xvel
+			self.y += self.yvel
+			self.yvel += self.yaccel
 			
 	def Draw(self):
 		win.blit(self.sprite, (self.x, self.y))
 		
-	
+
+		
 class Enemy:
-	def __init__(self, x, y, health, vel, enemtype, sprite):
+	def __init__(self, x, y, health, vel, enemtype, sprite, width, height):
 		
 		if enemtype == 1:
 			self.x = x
 			self.y = y
 			self.health = health
 			self.vel = vel
+			self.width = width
+			self.height = height
 			self.sprite = pygame.image.load(sprite)
-	
+			self.attacktype = 3
+			self.attacksprite = 'Enemy1Grenade.png'
+			self.attackCD = 1500
+			self.CDMAX = 1500
+			
 	def Draw(self):
 		win.blit(self.sprite, (self.x, self.y))
 		
 	def Move(self, x, y):
 		if self.x < x+150:
 			self.x += self.vel
-		elif self.x > x+350:
+		elif self.x > x+170:
 			self.x -= self.vel
 			
-		if self.y < y - 150:
+		if self.y < y - 20:
 			self.y += self.vel
 		elif self.y > y:
 			self.y -= self.vel
+
+
+def ColissionDetect(unitx, unity, unitwidth, unitheight, projectilex, projectiley):
+	xrange = range(unitx, (unitx + unitwidth))
+	yrange = range(unity, (unity + unitheight))
+
+	if projectilex in xrange and projectiley in yrange:
+		return True
+	else:
+		return False
+	
 		
 def redrawGameWindow():
 	global framecount
@@ -123,11 +159,17 @@ def redrawGameWindow():
 	for enemy in enemies:
 		enemy.Draw()
 	
+	for attack in enemyattacks:
+		attack.Draw()
+		
 	for bullet in bullets:
 		bullet.Draw()
 	
 	for bomb in bombs:
 		bomb.Draw()
+	
+	for explosion in explosions:
+		win.blit(pygame.image.load('Explosion.png'), explosion)
 	
 	pygame.display.update()
 	
@@ -137,8 +179,8 @@ run = True
 while run:
 	clock.tick(20)
 	
-	if bgx < -4500 and len(enemies) == 0:
-		Enemy1 = Enemy(1000, 350, 3, 4, 1, 'Enemy1.png')
+	if bgx < -2500 and len(enemies) == 0:
+		Enemy1 = Enemy(1000, 350, 3, 4, 1, 'Enemy1.png', 64, 64)
 		enemies.append(Enemy1)
 	
 	if bgx > -10230:
@@ -211,19 +253,59 @@ while run:
 			bullets.pop(bullets.index(bullet))
 	
 	for bomb in bombs:
-		if 0 < bomb.x < 1280 and 0 < bomb.y < 700:
+		if 0 < bomb.x < 1280 and bomb.y < 700:
 			bomb.Move()
 		else:
 			bombs.pop(bombs.index(bomb))
 			
 	for enemy in enemies:
 		enemy.Move(Plane.x,Plane.y)
+		if enemy.attackCD <= 0:
+			EnemyAttack = Projectile(enemy.x, enemy.y, enemy.attacktype, enemy.attacksprite)
+			enemyattacks.append(EnemyAttack)
+			enemy.attackCD = enemy.CDMAX
+		else:
+			enemy.attackCD -= 40
+	
+	for attack in enemyattacks:
+		if 0 < attack.x < 1280 and 0 < attack.y < 700:
+			attack.Move()
+			attack.Draw()
+		else:
+			enemyattacks.pop(enemyattacks.index(attack))
+	
+	for bullet in bullets:
+		for enemy in enemies:
+			if ColissionDetect(enemy.x, enemy.y, enemy.width, enemy.height, bullet.x, bullet.y):
+				Explosion = (bullet.x,bullet.y)
+				explosions.append(Explosion)
+				bullets.pop(bullets.index(bullet))
+				
+	for bomb in bombs:
+		for enemy in enemies:
+			if ColissionDetect(enemy.x, enemy.y, enemy.width, enemy.height, bomb.x, bomb.y):
+				Explosion = (bomb.x,bomb.y)
+				explosions.append(Explosion)
+				bombs.pop(bombs.index(bomb))
+				
+	for attack in enemyattacks:
+		if ColissionDetect(Plane.x, Plane.y, 64, 64, attack.x, attack.y):
+			Explosion = (attack.x,attack.y)
+			explosions.append(Explosion)
+			enemyattacks.pop(enemyattacks.index(attack))
+		for enemy in enemies:
+			if ColissionDetect(enemy.x, enemy.y, enemy.width, enemy.height, attack.x, attack.y):
+				Explosion = (attack.x,attack.y)
+				explosions.append(Explosion)
+				enemyattacks.pop(enemyattacks.index(attack))
+	
 	
 	redrawGameWindow()
-	
-	
 	if framecount + 1 > 24:
 		framecount = 0
+		
 	else:
 		framecount += 1
+	if framecount % 4 == 0:
+		explosions = []
 pygame.quit()
